a79d088c3405888ab1db3f3954cddbbfdfaa61b7
Bruno Casali <brunoocasali@gmail.com>
2014-04-27 17:33:35 -0300

    API its OK to use

--- lib/message.rb
+++ lib/message.rb
@@ -5,7 +5,15 @@ module SmileAPI
     def self.getMessage(entity)
       @coon = SmileAPI::Connection.new
       collection = @coon.database.collection "messages"
-      collection.find().each { |row| p row }
+      collection.find("type_message" => entity.type_message, "language" => entity.language,
+                      "keywords" => { '$in' => entity.keywords },
+                      "profile.config.system_type" => entity.profile.config.system_type,
+                      "profile.sex" => entity.profile.sex,
+                      "profile.personality" => { '$in' => entity.profile.personality.split(",") },
+                      "$and" => [
+                          "profile.birthday.start" => { '$lte' => entity.profile.birthday },
+                          "profile.birthday.end" => { '$gte' => entity.profile.birthday }
+                                ]).each { |row| p row["message"].gsub("@name", entity.profile.name) }
       @coon.connection.close
     end
 
@@ -16,5 +24,6 @@ module SmileAPI
         false
       end
     end
+
   end
 end
--- lib/teste.rb
+++ lib/teste.rb
@@ -5,7 +5,7 @@ require '/home/bruno/Cabuum/Projects/SmileAPI/smile_api/lib/message_entity.rb'
 require '/home/bruno/Cabuum/Projects/SmileAPI/smile_api/lib/type_message.rb'
 require '/home/bruno/Cabuum/Projects/SmileAPI/smile_api/lib/profile.rb'
 
-profile = SmileAPI::Profile.new "joão","animated","M", Time.new("2001-01-01")
-h = SmileAPI::MessageEntity.new "pt-br", ["banco", "conexão"], SmileAPI::TypeMessage::ERROR, profile
+profile = SmileAPI::Profile.new "John", "crazy", "M", Time.new("1996-01-01"), SmileAPI::Config.new("cabuum")
+h = SmileAPI::MessageEntity.new "pt-BR", [ "cadastro" ], SmileAPI::TypeMessage::SUCCESS, profile
 
 msg = SmileAPI::Message.getMessage h
